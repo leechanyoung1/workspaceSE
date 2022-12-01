a619e8811cb6bb982b0c6d90ccac7f27f6f7b73d
leechanyoung1 <cksdud6730@naver.com>
2022-12-02 03:42:31 +0400

    ..

--- 19.컬렉션(collection)/src/generic/Test.java
+++ 19.컬렉션(collection)/src/generic/Test.java
@@ -3,7 +3,14 @@ package generic;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
 
 	}
 
